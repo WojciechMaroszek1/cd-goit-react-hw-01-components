74a3b72c3e3b77294a2abb3e1e9366a6624d84eb
Wojciech Maroszek <122974346+WojciechMaroszek1@users.noreply.github.com>
2023-10-29 20:31:54 +0100

    feat: TransactionHistory

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import {Profile} from './Profile/Profile';
 import {Statistics} from './Statistic/Statistics';
 import {FriendList} from './FriendList/FriendList';
+import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 
 import user from '../data/user.json';
 import data from '../data/data.json';
 import friends from '../data/friends.json';
+import transactions from '../data/transactions.json';
+
 
 export const App = () => {
   return ( 
   <div 
     style ={{
+        paddingTop: 40,
+        paddingBottom: 40,
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
@@ -26,6 +31,7 @@ export const App = () => {
       />
     <Statistics title="Upload stats" stats={data} />
     <FriendList friends={friends}></FriendList>
+    <TransactionHistory items={transactions} />
 
   </div>
    );
